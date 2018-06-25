1021c7a9f84eac6bba708e21383f6b8d91076523
dy21335 <daisy_fyx@163.com>
2018-06-25 17:55:26 +0800

    完成dotLoading测试部分

--- test/unit/specs/dotLoading.spec.js
+++ test/unit/specs/dotLoading.spec.js
@@ -3,14 +3,20 @@ import dotLoading from '@/components/dotLoading'
 import { mount } from '@vue/test-utils'
 
 describe('dotLoading.vue', () => {
-  it('should render correct contents', () => {
-    const wrapper = mount(dotLoading)
-    wrapper.setProps({size: '25px', radius: 80, numbers: 16, color: '#abcdef'})
-    console.log(wrapper.vm.size)
+  it('should render correct width', () => {
+    const wrapper = mount(dotLoading, {
+      propsData: {size: '25px'}
+    })
     const liArray = wrapper.findAll('li')
-    console.log(liArray.at(0).vnode.data.style)
     for (let i = 0; i < liArray.length; ++i) {
-      console.log(liArray.at(i).element.style.width)
+      expect(liArray.at(i).element.style.width).toBe('25px')
     }
+  })
+  it('should render correct number', () => {
+    const wrapper = mount(dotLoading, {
+      propsData: {numbers: 18}
+    })
+    const liArray = wrapper.findAll('li')
+    expect(liArray.length).toBe(18)
   })
 })
