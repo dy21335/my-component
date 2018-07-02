225aa872c5c9345d3cc75252cc3afb6a8e7b3519
dy21335 <daisy_fyx@163.com>
2018-07-02 19:41:11 +0800

    RecycleListView

---
+++ src/components/RecycleListView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="test">
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RecycleListView'
+}
+</script>
+
+<style scoped>
+  .test{
+    width: 80px;
+    height: 100px;
+    background-color: #0074D9;
+    overflow: hidden;
+  }
+  ul{
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  li{
+    width: 100%;
+    height: 40px;
+    margin: 10px;
+    background-color: #7f7f7f;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import dotLoading from '@/components/dotLoading'
 import picHover from '@/components/picHover'
+import RecycleListView from '@/components/RecycleListView'
 
 Vue.use(Router)
 
@@ -14,9 +15,14 @@ export default new Router({
     },
     {
       path: '/picHover',
-      name: picHover,
+      name: 'picHover',
       component: picHover,
       props: {isShowing: true}
+    },
+    {
+      path: '/recycle',
+      name: 'RecycleListView',
+      component: RecycleListView
     }
   ]
 })
