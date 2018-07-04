1ad74f4dd064c6f4348f35d71db91815c7c92ab8
dy21335 <daisy_fyx@163.com>
2018-07-04 14:48:18 +0800

    RecycleListView组件初步实现

--- src/components/RecycleListView.vue
+++ src/components/RecycleListView.vue
@@ -1,36 +1,111 @@
 <template>
-  <div class="test">
-    <ul>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
+  <div ref="content" id="content" class="content">
+    <ul ref="list" class="list" >
+      <li ref="firstChid" id="fist-item"></li>
+      <li v-for="(item, key) in items" :key="key" class="item">
+        <SinglePage :imgSrc="item.src"></SinglePage>
+      </li>
+      <li ref="lastChild" class="last-child">
+        已经到底部啦~
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import SinglePage from '@/components/SinglePage'
 export default {
-  name: 'RecycleListView'
+  name: 'RecycleListView',
+  data () {
+    return {
+      items: [
+        { src: require('../assets/2.jpg') },
+        { src: require('../assets/2.jpg') },
+        { src: require('../assets/2.jpg') },
+        { src: require('../assets/2.jpg') },
+        { src: require('../assets/2.jpg') },
+        { src: require('../assets/2.jpg') },
+        { src: require('../assets/2.jpg') },
+        { src: require('../assets/2.jpg') },
+        { src: require('../assets/2.jpg') }
+      ],
+      elems: {
+        content: {},
+        list: {}
+      }
+    }
+  },
+  components: {
+    SinglePage
+  },
+  methods: {
+    //  行不通
+    init () {
+      this.elems.content = this.$refs.content
+      this.elems.list = this.$refs.list
+      console.log(this.$refs.hasOwnProperty('list'))
+    },
+    //  cubic函数
+    easeInOutQuint (t, b, c, d) {
+      t /= d / 2
+      if (t < 1) return c / 2 * t * t + b
+      t--
+      return -c / 2 * (t * (t - 2) - 1) + b
+    },
+    //  边缘回弹动画
+    edgeBounce (content) {
+      if (content.clientHeight + content.scrollTop < content.scrollHeight - 90) {
+      } else {
+        content.scrollTop -= this.easeInOutQuint(0.63, 0.9, 0.7, 1)
+        console.log(content.scrollTop)
+        window.requestAnimationFrame(this.edgeBounce.bind(null, content))
+      }
+    }
+  },
+  beforeMount () {
+    this.init()
+  },
+  mounted () {
+    var div = document.getElementById('content')
+    // 添加边缘回弹动画
+    div.addEventListener('scroll', (ev) => {
+      if (div.clientHeight + div.scrollTop >= div.scrollHeight) {
+        this.edgeBounce(div)
+      }
+    })
+  }
 }
 </script>
 
 <style scoped>
-  .test{
-    width: 80px;
-    height: 100px;
-    background-color: #0074D9;
+  .content{
+    position:relative;
+    margin: auto;
+    width: 500px;
+    height: 400px;
+    background-color: #FFCC99;
+    overflow: auto;
+
+  }
+
+  ul{
+    margin: auto;
+    text-align: center;
+    list-style: none;
+    padding: 0;
     overflow: hidden;
   }
-  ul{
-    list-style: none;
-    margin: 0;
-    padding: 0;
-  }
-  li{
-    width: 100%;
-    height: 40px;
+  .item{
+    position: relative;
+    height: 210px;
     margin: 10px;
-    background-color: #7f7f7f;
+    /*background-color: #FFCCCC;*/
+  }
+  img{
+    width: 300px;
+  }
+  .last-child{
+    text-align: center;
+    height: 70px;
   }
 </style>
---
+++ src/components/SinglePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div id="container">
+      <img :src="imgSrc"/>
+      <div id="downPage">
+        <p>
+          {{paragraph}}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SinglePage',
+  props: [
+    'imgSrc',
+    'paragraph'
+  ]
+}
+</script>
+
+<style scoped>
+  div{
+    height:100%;
+  }
+  img{
+    display: block;
+    max-width: 100%;
+    max-height: 100%;
+  }
+
+</style>
