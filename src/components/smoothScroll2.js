c67f83f6436a41bb3302171e4c443dfb6eb33c74
dy21335 <daisy_fyx@163.com>
2018-07-12 09:53:09 +0800

    缓动函数test

--- src/components/smoothScroll2.js
+++ src/components/smoothScroll2.js
@@ -1,69 +1,37 @@
+
 function smoothScroll (elem) {
-  function easeOutQuad(t, b, c, d) {
-    return -c *(t/=d)*(t-2) + b;
+  function easeOutQuad (t, b, c, d) {
+    return -c * (t /= d) * (t - 2) + b
   }
 
-  function normalSpeed(v, t, a) {
-    return v*t - 1/2*a*t*t
+  function normalSpeed (v, t, a) {
+    return v * t - 1 / 2 * a * t * t
   }
 
   var id
-  // var gapY = 100;
-
-  // function animateScrollTo (direction, elem, start) {
-  //   var lastTime = 0
-  //   var lastPos = 0
-  //   var index = 0
-  //   // console.log('lastTime', lastTime)
-  //   var t = 0, v = 60, a = 0.1;
-  //   t = v/a
-  //   console.log('t',t)
-  //
-  //   function animationScroll () {
-  //     index++
-  //     v = 60 - a * lastTime
-  //     // var delta = v * Math.exp(-elapsed / 325)
-  //     var currentPos = normalSpeed(60, lastTime, a)
-  //     lastTime = lastTime+1
-  //     var delta = (currentPos - lastPos)*0.03
-  //     lastPos = currentPos
-  //     // debugger
-  //     delta = direction > 0 ? delta : -delta
-  //     console.log(delta)
-  //     if ( v > 0 ){
-  //       // if(index%2==0){
-  //         elem.scrollTop += delta
-  //       // }
-  //       window.requestAnimationFrame(animationScroll)
-  //     } else {
-  //       console.log('end')
-  //       cancelAnimationFrame(id)
-  //     }
-  //   }
-  //   // animationScroll()
-  //   requestAnimationFrame(animationScroll)
-  // }
   function animateScrollTo (direction, elem, start) {
     var lastTime = 0
     var lastPos = 0
     var index = 0
     // console.log('lastTime', lastTime)
-    var t = 0, v = 60, a = 0.1;
-    t = v/a
-    console.log('t',t)
+    // var t = 0, vo = 30, a = 0.9；
+    var t = 0, vo = 30, a = 0.9
+    // 真正所需时间是t/60（因为每16毫秒调用一次）
+    t = vo / a
+    console.log('t', t)
 
     function animationScroll () {
       index++
-      v = 60 - a * lastTime
+      var vt = vo - a * lastTime
       // var delta = v * Math.exp(-elapsed / 325)
-      var currentPos = normalSpeed(60, lastTime, a)
-      lastTime = lastTime+1
-      var delta = (currentPos - lastPos)*0.03
+      var currentPos = normalSpeed(vo, lastTime, a)
+      lastTime = lastTime + 1
+      var delta = (currentPos - lastPos)
       lastPos = currentPos
       // debugger
       delta = direction > 0 ? delta : -delta
       console.log(delta)
-      if ( v > 0 ){
+      if (vt > 0) {
         // if(index%2==0){
         elem.scrollTop += delta
         // }
